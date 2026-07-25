c022588eb04f1b48993137026f7970993f6a12bd
AdiJoshi29 <hi2adityajoshi@gmail.com>
2026-07-25 22:18:40 +0530

    Initial Commit

---
+++ js/memories.js
@@ -0,0 +1,479 @@
+/* ==========================================================
+   OUR ATLAS
+   memories.js
+========================================================== */
+
+window.memories = [
+
+    {
+        id: 1,
+
+        city: "Pune",
+        location: "Pune, India",
+
+        lat: 18.5204,
+        lng: 73.8567,
+
+        image: "photos/pune.jpg",
+
+        date: "2024",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 2,
+
+        city: "Goa",
+        location: "Goa, India",
+
+        lat: 15.4909,
+        lng: 73.8278,
+
+        image: "photos/goa.jpg",
+
+        date: "Summer 2024",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 3,
+
+        city: "Mumbai",
+        location: "Mumbai, India",
+
+        lat: 19.0760,
+        lng: 72.8777,
+
+        image: "photos/mumbai.jpg",
+
+
+        date: "2024",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 4,
+
+        city: "Delhi",
+        location: "Delhi, India",
+
+        lat: 28.6139,
+        lng: 77.2090,
+
+        image: "photos/delhi.jpeg",
+
+        date: "Winter 2025",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 5,
+
+        city: "Port Moresby",
+        location: "Port Moresby, Papua New Guinea",
+
+        lat: -9.4865,
+        lng: 147.1540,
+
+        image: "photos/portmoresby.jpg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 6,
+
+        city: "Melbourne",
+        location: "Melbourne, Australia",
+
+        lat: -37.8136,
+        lng: 144.9631,
+
+        image: "photos/melbourne.jpeg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+    
+
+    {
+        id: 7,
+
+        city: "Angola",
+        location: "Luanda, Angola",
+
+        lat: -8.8361,
+        lng: 13.2344,
+
+        image: "photos/angola.jpg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 8,
+
+        city: "Paris",
+        location: "Paris, France",
+
+        lat: 48.8566,
+        lng: 2.3522,
+
+        image: "photos/paris.jpg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 9,
+
+        city: "Nanital",
+        location: "Nanital, India",
+
+        lat: 30.8667,
+        lng: 79.9333,
+
+        image: "photos/nanital.jpg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 10,
+
+        city: "Assam",
+        location: "Assam, India",
+
+        lat: 26.7682,
+        lng: 94.2380,
+
+        image: "photos/assam.jpeg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 11,
+
+        city: "Indore",
+        location: "Indore, India",
+
+        lat: 22.5697,
+        lng: 75.8898,
+
+        image: "photos/indore.jpeg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 12,
+
+        city: "Athens",
+        location: "Athens, Georgia, USA",
+
+        lat: 33.9391,
+        lng: -84.5247,
+
+        image: "photos/athens.jpeg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 13,
+
+        city: "Atlanta",
+        location: "Atlanta, Georgia, USA",
+
+        lat: 33.7490,
+        lng: -84.3880,
+
+        image: "photos/atlanta.jpeg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 14,
+
+        city: "Grand Canyon",
+        location: "Grand Canyon, Arizona, USA",
+
+        lat: 36.0544,
+        lng: -112.1401,
+
+        image: "photos/grandcanyon.jpeg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 15,
+
+        city: "New York",
+        location: "New York, NY, USA",
+
+        lat: 40.7128,
+        lng: -74.0060,
+
+        image: "photos/newyork.jpg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 16,
+
+        city: "Los Angeles",
+        location: "Los Angeles, CA, USA",
+
+        lat: 34.0522,
+        lng: -118.2437,
+
+        image: "photos/losangeles.jpeg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 17,
+
+        city: "Phoenix",
+        location: "Phoenix, Arizona, USA",
+
+        lat: 33.4484,
+        lng: -112.0740,
+
+        image: "photos/phoenix.jpeg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 18,
+
+        city: "Niagara Falls",
+        location: "Buffalo, New York, USA",
+
+        lat: 43.0962,
+        lng: -79.0750,
+
+        image: "photos/niagarafalls.jpg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+
+    {
+        id: 19,
+
+        city: "Singapore",
+        location: "Singapore, Singapore",
+
+        lat: 1.3521,
+        lng: 103.8198,
+
+        image: "photos/singapore.jpeg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 20,
+
+        city: "Malaysia",
+        location: "Kuala Lumpur, Malaysia",
+
+        lat: 3.1390,
+        lng: 101.6869,
+
+        image: "photos/malaysia.jpeg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 21,
+
+        city: "Kashmir",
+        location: "Kashmir, India",
+
+        lat: 34.0816,
+        lng: 74.8539,
+
+        image: "photos/kashmir.jpg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+
+    {
+        id: 22,
+
+        city: "Mysore",
+        location: "Mysore, Karnataka, India",
+
+        lat: 12.3059,
+        lng: 74.2086,
+
+        image: "photos/mysore.jpeg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 23,
+
+        city: "Congo",
+        location: "Congo, Democratic Republic of the Congo",
+
+        lat: -0.3816,
+        lng: 15.4440,
+
+        image: "photos/congo.jpg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 24,
+
+        city: "Tunisia",
+        location: "Tunisia, Tunisia",
+
+        lat: 36.8065,
+        lng: 10.1815,
+
+        image: "photos/tunisia.jpg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+    {
+        id: 25,
+
+        city: "Orlando",
+        location: "Orlando, Florida, USA",
+
+        lat: 28.5383,
+        lng: -81.3792,
+
+        image: "photos/orlando.jpg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+   {
+        id: 26,
+
+        city: "Chicago",
+        location: "Chicago, Illinois, USA",
+
+        lat: 41.8781,
+        lng: -87.6298,
+
+        image: "photos/chicago.jpg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    },
+
+   {
+        id: 27,
+
+        city: "Cameroon",
+        location: "Cameroon, Africa",
+
+        lat: 3.8480,
+        lng: 11.5079,
+
+        image: "photos/cameroon.jpg",
+
+        date: "2025",
+
+        color: "#C79C32"
+    }    
+];
+
+
+/* ==========================================================
+   SITE SETTINGS
+========================================================== */
+
+window.siteSettings = {
+
+    /* Website Title */
+
+    title: "Happy Birthday ❤️",
+
+    /* Small text above the title */
+
+    subtitle: "YOUR ATLAS",
+
+    /* Footer quote */
+
+    footer:
+        "Every destination became another chapter of your story.",
+
+    /* Marker Appearance */
+
+    markerColor: "#C79C32",
+
+    markerAltitude: 0.015,
+
+    markerRadius: 0.35,
+
+    /* Globe Behaviour */
+
+    autoRotate: false,
+
+    initialAltitude: 2.2
+
+};
